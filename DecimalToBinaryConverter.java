6009e7db005ea85283d451f49ceb509bc715567a
Mladen Petrov <mladen.petrov88@gmail.com>
2020-01-19 15:02:04 +0200

    Add files via upload

---
+++ DecimalToBinaryConverter.java
@@ -0,0 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class DecimalToBinaryConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        if (input == 0) {
+            System.out.println("0");
+        }
+
+        while (input > 0) {
+            stack.push(input % 2);
+            input /= 2;
+        }
+
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
+            System.out.print(stack.pop());
+        }
+    }
+}
